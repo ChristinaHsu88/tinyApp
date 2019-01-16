43ccc9508626e6ec15b058bd7178f5bac2f07823
Christina <christina.hsu.ca@gmail.com>
2019-01-16 23:11:24 +0000

    updated express_server.js file before cookie

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase }; //set up the object for import
+    let templateVars = { urls: urlDatabase }; //set up the object so that the ejs file can access the database via the key "urls"
     res.render("urls_index", templateVars);
 });
 
@@ -30,9 +30,14 @@ app.get("/urls/new", (req, res) => {  //new submission form - and the path /urls
 });
 
 app.get("/urls/:id", (req, res) => { //this will match the new form. 
-    let templateVars = { shortURL: req.params.id };
+    var id = req.params.id;
+    let templateVars = { 
+        shortURL: req.params.id,
+        url: urlDatabase[id]
+    }; //so that the ejs can access value here. create scope for ejs
     res.render("urls_show", templateVars);
 });
+
 
 app.post("/urls", (req, res) => {
     //console.log(req.body);      // debug statement to see POST parameters
@@ -46,7 +51,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/hello", (req, res) => {
     let templateVars = { greeting: 'Hello World!'};
-    res.render("hello_world", templateVars);
+    res.render("hello_world", templateVars);  //render the "hello_world" file, with the value stored in templateVars
 });
 
 app.get("/u/:shortURL", (req, res) => { //if /path/: exist already. need another action name under get method 
@@ -57,6 +62,18 @@ app.get("/u/:shortURL", (req, res) => { //if /path/: exist already. need another
 
 })
 
+app.post("/urls/:id/delete", (req, res) => {
+    const id = req.params.id; //id corresponds to the path above
+    delete urlDatabase[id]; //get the data from id variable.
+    res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+    var update = req.body.input;
+    var id = req.params.id;
+    urlDatabase[id] = update;
+    res.redirect("/urls");
+});
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
